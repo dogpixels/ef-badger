bea10dda7f07efb77f3a53cf74f3f744b4023914
draconigen <draconigen@gmail.com>
2023-04-12 12:41:16 +0200

    feat: :white_check_mark: connect to modified.json
    The observed caching behavior during the beta has been mitigated by altering the EFWebCore framework  to move the last modified timestamps to a separate file, modified.json, and set up the server to disallow caching of that file.  Quick tests suggest that the desired behavior has now been achieved.

--- README.md
+++ README.md
@@ -5,14 +5,14 @@ Enhances your visit to the Eurofurence website by adding `new` badges to pages t
 * Firefox 57+ or
 * Chrome
 
-> Note that this addon only works with the **main** website starting EF27, and only for pages hosted on the main website itself. Any other site and service under Eurofurence that is **not** on the main website (e.g. looks different) is not supported.
+> Note that this addon only works with the **main** website (address starting with `www.`), and only for pages hosted on the main website itself. Any other site and service under Eurofurence that is **not** on the main website (address does not start with `www.`) is not supported.
 
 ## Installation
 * **Firefox**: Go to https://addons.mozilla.org/firefox/addon/eurofurence-badger/ and click "Add to Firefox".
 * **Chrome**: Go to https://chrome.google.com/webstore/detail/eurofurence-badger/onjgbdbgijjcpheemlgglchgcoaacknl and click "Add to Chrome".
 
-## Feedback & Contributions
-Feedback welcome via Telegram: https://t.me/draconigen or eMail: draconigen@gmail.com
+## Support, Feedback & Contributions
+Join the Telegram Support Group: https://t.me/dogpixels.
 
 You know JavaScript and want to improve this Extension? Pull Requests to this repository are always welcome.
 
--- chrome/js/www.eurofurence.org.js
+++ chrome/js/www.eurofurence.org.js
@@ -3,7 +3,7 @@
  * 	draconigen@gmail.com
  */
 
-const debug = true;
+const debug = false;
 
 async function storage_get()
 {
@@ -21,43 +21,62 @@ document.body.addEventListener("newsLoaded", async () =>
 		return;
 	window.badgerized = true;
 
-	var data = await storage_get();
-	if (debug) console.log(`[ef-badger] data loaded from storage:`, data);
+	const base = document.getElementsByTagName("base")[0].href;
 
-	document.querySelectorAll('[data-lastmodified]').forEach(element =>
+	var local_data = await storage_get();
+	if (debug) console.log(`[ef-badger] local data loaded from storage:`, local_data);
+
+	var server_data = null;
+
+	try
 	{
-		const page = element.href;
-		const mod = element.dataset.lastmodified;
-		const active = element.classList.contains('ef-active');
+		server_data = await (await fetch(base + "modified.json")).json();
 
-		if (!(page in data))
+		if (!server_data)
 		{
-			if (debug) console.info(`page not in storage, adding ${page}`);
-			element.classList.add('ef-new');
-			data[page] = mod;
+			console.info('[ef-badger] server_data', server_data);
+			throw "malformed server_data";
 		}
-		else if (mod > data[page])
-		{
-			if (debug) console.info(`${mod} > ${data[page]} -> decorating`)
-			{
-				element.classList.add('ef-new');
 
-				element.addEventListener('click', async () => 
-				{
-					if (debug) console.info(`user clicked on badge, updating ${page}`);
-					data[page] = mod;
-					await storage_set(data);
-					element.classList.remove('ef-new');
-				});
-			}
+		if (debug) console.log(`[ef-badger] server data loaded from ${base + "modified.json"}:`, server_data);
+	}
+	catch(ex)
+	{
+		console.error(`[ef-badger] failed to load "${base + "modified.json"}", reason: ${ex}`);
+		return;
+	}
+
+	document.querySelectorAll('nav a:not([href^="http"])').forEach(element =>
+	{
+		const key = element.href.replace(base, '');
+		const active = element.classList.contains('ef-active');
+		const mod = server_data[key];
+
+		if (!(key in local_data))
+		{
+			if (debug) console.info(`[ef-badger] "${key}" not in local storage, adding "${key}": ${mod}`);
+			element.classList.add('ef-new');
+			local_data[key] = mod;
+		}
+		else if (mod > local_data[key])
+		{
+			if (debug) console.info(`[ef-badger] ${mod} > ${local_data[key]} -> decorating`);
+			element.classList.add('ef-new');
+			element.addEventListener('click', async () => 
+			{
+				if (debug) console.info(`[ef-badger] user clicked on badge, updating "${key}" in local storage`);
+				local_data[key] = mod;
+				await storage_set(local_data);
+				element.classList.remove('ef-new');
+			});
 		}
 
 		if (active)
 		{
-			if (debug) console.info(`updating ${mod} for active page ${page}`);
-			data[page] = mod;
+			if (debug) console.info(`[ef-badger] updating ${mod} for active page "${key}"`);
+			local_data[key] = mod;
 		}
 	});
 
-	await storage_set(data);
+	await storage_set(local_data);
 });
